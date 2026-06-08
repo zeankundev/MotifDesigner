acd0b24392e9b37803fe80035671a83ee15d1c97
Zean Isnomo <zeanfender11@gmail.com>
2026-06-08 12:45:06 +0700

    Get the essentials running up and as well as CMake bootstrap

--- shortcuts.sh
+++ shortcuts.sh
@@ -3,11 +3,21 @@
 build() {
     echo "Making sure you have the dependencies"
     DOES_CMAKE_EXIST=$(which cmake)
-    if [ $DOES_CMAKE_EXIST != ""]; then
+    if [ $DOES_CMAKE_EXIST != "" ]; then
         echo "Great, you have cmake. Now building..."
-
+        cmake -S . -B build
+        cmake --build build
     fi
 }
 
-if [ $1 = ""]; then
+if [ $1 = "" ]; then
     echo "Please type either: build, clean, quick."
+elif [ $1 = "build" ]; then
+    build
+elif [ $1 = "clean" ]; then
+    echo "Cleaning up..."
+    rm -rf build
+elif [ $1 = "quick" ]; then
+    rm -rf build
+    build
+fi
