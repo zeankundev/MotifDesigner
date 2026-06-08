f5314669f0748770e2986e0116ee5cfac5f96473
Zean Isnomo <zeanfender11@gmail.com>
2026-06-08 14:49:06 +0700

    Added X11 testing core to test most stuffs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#include <Xm/Xm.h>
+#include <Xm/Form.h>
 #include "IMCPasser.h"
+#include <X11/Shell.h>
+#include <Xm/PushB.h>
+#include <X11/Composite.h>
+#include <Xm/XmStrDefs.h>
+#include <X11/Intrinsic.h>
 
 using namespace std;
 
-int main() {
-    cout << "Test" << endl;
-    IMCPasser::TestStdOut("Hello to IMCPasser from main!");
+void ButtonPushCallback(Widget w, XtPointer clientData, XtPointer callData) {
+    IMCPasser::TestStdOut("Hello while clicking!");
+}
+
+Widget InitializeUI(Widget parent) {
+    Arg args[10];
+    int n = 0;
+    Widget mainLayout = XmCreateForm(parent, (char*)"mainLayout", args, 0);
+    XtManageChild(mainLayout);
+    /* Attach the button to the form and set offsets. Form ignores XmNx/XmNy so use
+       attachment + offsets to control absolute positioning inside the Form. */
+    XtSetArg(args[n], XmNx, 30); n++;
+    XtSetArg(args[n], XmNy, 30); n++;
+    XtSetArg(args[n], XmNwidth, 200); n++;
+    XtSetArg(args[n], XmNheight, 30); n++;
+    XtSetArg(args[n], XmNlabelString, XmStringCreateLocalized((char*)"Send to IMC passer as test")); n++;
+    Widget testButton = XmCreatePushButton(mainLayout, (char*)"testButton", args, n);
+    XtManageChild(testButton);
+    XtAddCallback(testButton, XmNactivateCallback, ButtonPushCallback, NULL);
+    return mainLayout;
+}
+
+int main(int argc, char **argv) {
+    XtAppContext context;
+    Widget shell = XtVaOpenApplication(
+        &context, "MotifDesigner",
+        NULL, 0, &argc, argv, NULL,
+        sessionShellWidgetClass,
+        XmNwidth, 300,
+        XmNheight, 300,
+        NULL);
+    InitializeUI(shell);
+    XtRealizeWidget(shell);
+    XtAppMainLoop(context);
     return 0;
 }
