1b0fd0d00a6eb37cf76b90a5665d786fef39892e
Zean Isnomo <zeanfender11@gmail.com>
2026-06-09 15:58:55 +0700

    Added better toolbar iconing method which defeats the OOP purpose

--- src/components/Toolbar.cpp
+++ src/components/Toolbar.cpp
@@ -8,6 +8,29 @@
 #include <Xm/PushB.h>
 #include <Xm/Xm.h>
 #include <Xm/XmStrDefs.h>
+#include <iostream>
+
+class ToolbarButtons {
+    public:
+        static void CreateToolbarButton(Widget parent, char **xpmData, char *name, void (*callback)(Widget, XtPointer, XtPointer)) {
+            Arg args[10];
+            int n = 0;
+            Pixmap pixmap = PixmapManager::XpmToPixmap(parent, xpmData);
+            if (pixmap == None) return;
+            XtSetArg(args[n], XmNlabelType, XmPIXMAP); n++;
+            XtSetArg(args[n], XmNlabelPixmap, pixmap); n++;
+            XtSetArg(args[n], XmNwidth, 30); n++;
+            XtSetArg(args[n], XmNheight, 30); n++;
+            XtSetArg(args[n], XmNalignment, XmALIGNMENT_CENTER); n++;
+            Widget button = XmCreatePushButton(parent, name, args, n);
+            XtAddCallback(button, XmNactivateCallback, callback, NULL);
+            XtManageChild(button);
+        }
+};
+
+void SelectButtonCallback(Widget widget, XtPointer clientData, XtPointer callData) {
+    std::cout << "Test select?" << std::endl;
+}
 
 Widget Components::RenderToolbar(Widget parent) {
     Arg args[10];
@@ -17,32 +40,18 @@ Widget Components::RenderToolbar(Widget parent) {
     XtSetArg(args[n], XmNtopAttachment, XmATTACH_WIDGET); n++;
     XtSetArg(args[n], XmNbottomAttachment, XmATTACH_WIDGET); n++;
     XtSetArg(args[n], XmNleftAttachment, XmATTACH_WIDGET); n++;
+    XtSetArg(args[n], XmNwidth, 40); n++;
     Widget mainToolbar = XmCreateFrame(parent, (char*)"LeftToolbar", args, n);
     XtManageChild(mainToolbar);
 
     n = 0;
+    /* Add inner padding so children inside the Form have breathing room. */
+    XtSetArg(args[n], XmNmarginWidth, 6); n++;
+    XtSetArg(args[n], XmNmarginHeight, 6); n++;
     Widget toolbarForm = XmCreateForm(mainToolbar, (char*)"ToolbarForm", args, n);
     XtManageChild(toolbarForm);
 
-    n = 0;
-    Pixmap selectIcon = PixmapManager::XpmToPixmap(parent, SelectIcon);
-    XtSetArg(args[n], XmNlabelType, XmPIXMAP); n++;
-    XtSetArg(args[n], XmNlabelPixmap, selectIcon); n++;
-    XtSetArg(args[n], XmNalignment, XmALIGNMENT_CENTER); n++;
-    XtSetArg(args[n], XmNwidth, 28); n++;
-    XtSetArg(args[n], XmNheight, 28); n++;
-    Widget selectButton = XmCreatePushButton(toolbarForm, (char*)"SelectButton", args, n);
-    XtManageChild(selectButton);
-
-    n = 0;
-    XtSetArg(args[n], XmNwidth, 28); n++;
-    XtSetArg(args[n], XmNheight, 28); n++;
-    XtSetArg(args[n], XmNtopAttachment, XmATTACH_WIDGET); n++;
-    XtSetArg(args[n], XmNtopWidget, selectButton); n++;
-    XtSetArg(args[n], XmNtopOffset, 10); n++;
-    XtSetArg(args[n], XmNlabelString, XmStringCreateLocalized((char*)"S")); n++;
-    Widget somethingButton = XmCreatePushButton(toolbarForm, (char*)"GenericButtonButton", args, n);
-    XtManageChild(somethingButton);
+    ToolbarButtons::CreateToolbarButton(toolbarForm, SelectIcon, (char*)"SelectButton", SelectButtonCallback);
 
     return mainToolbar;
 }
